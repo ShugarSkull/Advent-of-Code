d6b05a4d2eb64c727384470b4d983d2f8b467d80
ShugarSkull <zero.exe@hotmail.fr>
2022-12-05 19:32:50 +0100

    Jour2 terminé

--- 2022/Jour2.py
+++ 2022/Jour2.py
@@ -1,3 +1,6 @@
+# On pourait faire en sorte d'améliorer la lisibilité du code en augmentant
+# le niveau d'abstaction
+
 from libaoc import read_input
 
 input = read_input('2022/Jour2.input')
@@ -56,3 +59,37 @@ def parse_inputV2(input: list[str]) -> list[int]:
     for row in input.split('\n'):
         score_list.append(0)
         round = row.split(' ')
+        match round[1]:
+            case 'X':
+                score_list[i] += 0
+                match round[0]:
+                    case 'A':
+                        score_list[i] += 3
+                    case 'B':
+                        score_list[i] += 1
+                    case 'C':
+                        score_list[i] += 2
+            case 'Y':
+                score_list[i] += 3
+                match round[0]:
+                    case 'A':
+                        score_list[i] += 1
+                    case 'B':
+                        score_list[i] += 2
+                    case 'C':
+                        score_list[i] += 3
+            case 'Z':
+                score_list[i] += 6
+                match round[0]:
+                    case 'A':
+                        score_list[i] += 2
+                    case 'B':
+                        score_list[i] += 3
+                    case 'C':
+                        score_list[i] += 1
+        i += 1
+    return score_list
+
+score_listV2 = parse_inputV2(input)
+
+print(add_score(score_listV2))
